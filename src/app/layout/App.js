770091fb762037b1aaa964938c11f2122267db51
Brian Kang <brian@Brians-MacBook-Pro.local>
2022-06-15 00:54:36 +1000

    end of section 1 : set up new project GoFishing

---
+++ src/app/layout/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>Event - Go Fish !! Add new text. </header>
+    </div>
+  );
+}
+
+export default App;
